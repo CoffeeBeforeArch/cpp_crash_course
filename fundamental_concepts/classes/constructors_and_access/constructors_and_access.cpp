9ce44c332bd9da9910a0a9e21b81229e2af5e2aa
Nick <coffeebeforearch@gmail.com>
2019-02-23 22:53:08 -0500

    Add constructor and access permission example

---
+++ fundamental_concepts/classes/constructors_and_access/constructors_and_access.cpp
@@ -0,0 +1,24 @@
+// This program shows off basic classes in C++
+// By: Nick from CoffeeBeforeArch
+
+#include <iostream>
+
+#include "interface.h"
+
+using namespace std;
+
+int main(){
+    // Create two vectors with two different constructors
+    vector v1;
+    vector v2(1.0, 2.0, 3.0, 4.0);
+
+    // Print out both vectors
+    v1.print();
+    v2.print();
+
+    // Update the empty vector and display value
+    v1.update(5.0, 5.0, 7.0, 7.0);
+    v1.print();
+
+    return 0;
+}
