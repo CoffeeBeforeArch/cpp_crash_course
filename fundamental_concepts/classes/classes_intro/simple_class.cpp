2f9125628e3aff4ebdc195dbb3ff01f7262ef5eb
Nick <coffeebeforearch@gmail.com>
2019-02-23 21:28:17 -0500

    add first OO example

---
+++ fundamental_concepts/classes/classes_intro/simple_class.cpp
@@ -0,0 +1,71 @@
+// This program implements a vector using using a class
+// By: Nick from CoffeeBeforeArch
+
+#include <iostream>
+
+using namespace std;
+
+// A vector class that has all coordinates
+class vector1{
+public:
+    double start_x;
+    double start_y;
+    double end_x;
+    double end_y;
+
+    // Prototype for print function
+    void print();
+};
+
+// A point class that contains a single coordinate pair
+class point{
+public:
+    double x;
+    double y;
+};
+
+// A vector class that has coordinates as point objects
+class vector2{
+public:
+    point start;
+    point end;
+};
+
+// Normal function that prints the starting and ending points of a vector
+void print_vector1(vector1 v){
+    cout << "Start x: " << v.start_x << ", Start y: " << v.start_y << endl;
+    cout << "End x: " << v.end_x << ", End y: " << v.end_y << endl;
+}
+
+// Method associated with the vector1 to print out the vector
+void vector1::print(){
+    cout << "Start x: " << start_x << ", Start y: " << start_y << endl;
+    cout << "End x: " << end_x << ", End y: " << end_y << endl;
+}
+
+int main(){
+    // Represent a vector (1.0, 2.0), (3.0, 4.0) using a single object
+    vector1 single_object;
+    single_object.start_x = 1.0;
+    single_object.start_y = 2.0;
+    single_object.end_x = 3.0;
+    single_object.end_y = 4.0;
+
+    // Pass the object to a function
+    print_vector1(single_object);
+
+    // Print using a method
+    single_object.print();
+
+    // Represent a vector using multiple objects
+    vector2 multiple_objects_1;
+    multiple_objects_1.start.x = 1.0;
+    multiple_objects_1.start.y = 2.0;
+    multiple_objects_1.end.x = 3.0;
+    multiple_objects_1.end.y = 4.0;
+
+    // Copy a vector
+    vector2 multiple_objects_2 = multiple_objects_1;
+
+    return 0;
+}
