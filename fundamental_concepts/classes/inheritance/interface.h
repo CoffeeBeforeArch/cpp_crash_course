b94c3b467f0c252428f0311b0fc798b4a7fb4520
Nick <CoffeeBeforeArch@gmail.com>
2019-03-02 14:40:15 -0500

    Add full inheritance example

--- fundamental_concepts/classes/inheritance/interface.h
+++ fundamental_concepts/classes/inheritance/interface.h
@@ -1,12 +1,44 @@
 // This file contains the interface for our inheritance example
 // By: Nick from CoffeeBeforeArch
 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
 class Vehicle {
 // May be accessed from within the class, or inherited classes
 protected:
+    string license;
     int year;
-    int vin;
 public:
-    // Constructor with initialization list
-    vehicle(const int &myYear, const int &myVIN)
-        : year(myYear), vin(myVIN) {}
+    // Constructor with initialization list (needed for constants)
+    Vehicle(const string &myLicense, const int &myYear)
+        : license(myLicense), year(myYear) {}
+    const string &getLicense() const {return license;}
+    const int &getYear() const {return year;}
+};
+
+// Car class inherits from Vehicle class
+// Public lets us keep original access specifiers
+class Car : public Vehicle {
+private:
+    string style;
+public:
+    // Another initialization list
+    Car(const string &myLicense, const int myYear, const string myStyle)
+        : Vehicle(myLicense, myYear), style(myStyle) {}
+    const string &getStyle() {return style;}
+};
+
+// Truck class that also inherits from Vehicle
+class Truck : public Vehicle {
+private:
+    int bed_length;
+public:
+    // Another initialization list
+    Truck(const string &myLicense, const int myYear, const int myBedLength)
+        : Vehicle(myLicense, myYear), bed_length(myBedLength) {}
+    const int &getBedLength() {return bed_length;}
+};
+
