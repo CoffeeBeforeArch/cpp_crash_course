5005a512345fb35691c37489a8ec42aa4d87f0d5
Nick <CoffeeBeforeArch@gmail.com>
2019-03-09 16:49:19 -0500

    add shell for multiple template implementation example

---
+++ fundamental_concepts/templates/multi_template_impl/multi_template_impl.cpp
@@ -0,0 +1,13 @@
+// This program shows how to have different implementations of a C++
+// Class using a single template
+// By: Nick from CoffeeBeforeArch
+
+#include <iostream>
+
+#include "interface.h"
+
+using namespace std;
+
+int main(){
+    
+}
