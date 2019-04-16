9f540257df098b21e640470652154da2110e0f2c
Nick <CoffeeBeforeArch@gmail.com>
2019-04-15 23:13:47 -0400

    Expand on smart pointer example

--- standard_library/smart_pointers/smart_pointers.cpp
+++ standard_library/smart_pointers/smart_pointers.cpp
@@ -13,10 +13,32 @@ int main(){
     shared_ptr<int> shared_int_1(new int(5));
     shared_ptr<int> shared_int_2 = shared_int_1;
 
+    // Print out use cout
+    cout << "Use count of shared_int: " << shared_int_1.use_count() << endl;
+
     // Create a weak pointer
     // Weak pointers point to memory, but are not an "owner"
     // Good when we don't want an owner, or handling cyclic references
     weak_ptr<int> weak_int = shared_int_1;
+
+    // Print out use cout
+    cout << "Use count of shared_int: " << weak_int.use_count() << endl;
+
+    // Convert the weak pointer to a shared pointer and print again 
+    auto shared_int_3 = weak_int.lock();    
+    cout << "Use count of shared_int: " << shared_int_3.use_count() << endl;
+
+    // Destroy the memory for 2 of the shared pointers and check uniqueness
+    shared_int_1.reset();
+    shared_int_2.reset();
+    cout << "Use count of shared_int: " << shared_int_3.use_count() << endl;
+    cout << "shared_int_3 is unique?: " << shared_int_3.unique() << endl;
+
+    // Reset the last pointer
+    shared_int_3.reset();
+
+    // Check to see if it is valid
+    cout << "shared int valid?: " << shared_int_3.get() << endl;
 
     // Create a unique pointer
     // Unique pointers, but are the only owner of the memory it points to
