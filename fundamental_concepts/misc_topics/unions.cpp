9ef421cf57aade4ab986fb508fd26d0118086552
Nick <coffeebeforearch@gmail.com>
2019-04-18 11:03:46 -0400

    Add full union example

--- fundamental_concepts/misc_topics/unions.cpp
+++ fundamental_concepts/misc_topics/unions.cpp
@@ -5,6 +5,7 @@
 
 using namespace std;
 
+// Simple struct definition that contains 4 different data types
 struct various_data{
     // 1 Byte
     char a;
@@ -16,6 +17,8 @@ struct various_data{
     double d;
 };
 
+// Create a simple Union
+// Looks like a struct, but there is only ever 1 valid field at a time
 union union_test{
     various_data union_struct;
     int union_int;
@@ -31,12 +34,26 @@ int main(){
     v_data.c = 867.5309;
     v_data.d = 2001.1002;
 
+    // Print out the size of a struct
+    cout << "sizeof struct: " << sizeof(various_data) << endl;
+    
+    // Create a union, and assign a field
     union_test ut_1;
     ut_1.union_struct = v_data;
 
-    cout << "sizeof struct: " << sizeof(v_data) << " pointer: " << &ut_1.union_struct << endl;
+    // Print out the size of the union
+    cout << "sizeof union: " << sizeof(ut_1) << endl;
+    
+    // Print out the address of the union, and of the field
+    cout << "Union pointer: " << &ut_1 << ", Field pointer: " << &(ut_1.union_struct) << endl;
 
+    // Re-assign the union to contain an integer now
     int b = 5;
-    ut_1.b = b;
+    ut_1.union_int = b;
+
+    // Print size of the union and pointers again
+    cout << "sizeof union: " << sizeof(ut_1) << endl;
+    cout << "Union pointer: " << &ut_1 << ", Field pointer: " << &(ut_1.union_int) << endl;
+
     return 0;
 }
