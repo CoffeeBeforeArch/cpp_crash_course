afcf96409df0e7bf3a2d252de2c923b609de5edc
Nick <CoffeeBeforeArch@gmail.com>
2019-07-15 19:06:53 -0400

    Add header guards

--- fundamental_concepts/misc_topics/header_guards/square.h
+++ fundamental_concepts/misc_topics/header_guards/square.h
@@ -1,5 +1,8 @@
 // This file contains a function for computing the square of a number
 // By: Nick from CoffeeBeforeArch
+#ifndef SQUARE_H
+#define SQUARE_H
 int square(int a){
     return a * a;
 }
+#endif
