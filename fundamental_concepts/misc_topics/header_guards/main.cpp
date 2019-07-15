c2fac6881145c78c5092489929d52256be3fb82b
Nick <coffeebeforearch@gmail.com>
2019-07-15 16:03:33 -0400

    Add header guard example

---
+++ fundamental_concepts/misc_topics/header_guards/main.cpp
@@ -0,0 +1,19 @@
+// This program shows how to handle duplicate definitions using header
+// guards
+// By: Nick from CoffeeBeforeArch
+#include <iostream>
+#include "square.h"
+#include "cube.h"
+
+using namespace std;
+
+int main(){
+    // Compute the square of some number "N"
+    int N = 3;
+    cout << "The square of " << N << " is " << square(N) << endl;
+    
+    // Compute the cube of some number "N"
+    cout << "The cube of " << N << " is " << cube(N) << endl;
+
+    return 0;
+}
---
+++ fundamental_concepts/misc_topics/header_guards/square.h
@@ -0,0 +1,5 @@
+// This file contains a function for computing the square of a number
+// By: Nick from CoffeeBeforeArch
+int square(int a){
+    return a * a;
+}
