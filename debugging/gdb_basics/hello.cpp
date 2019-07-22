1deec0ab0aec61b094642dc6174def29b43c05b3
Nick <CoffeeBeforeArch@gmail.com>
2019-07-22 17:24:37 -0400

    Add simple hello world example to step through code in gdb

---
+++ debugging/gdb_basics/hello.cpp
@@ -0,0 +1,15 @@
+// This program is for use in showing the basics of moving through
+// programs in GDB
+// By: Nick from CoffeeBeforeArch
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    const string s("Hello, world!");
+    cout << s << endl;
+    return 0;
+}
+
