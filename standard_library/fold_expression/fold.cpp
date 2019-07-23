eb5b210cfdbfe13d25f570732de17743061de50c
Nick <CoffeeBeforeArch@gmail.com>
2019-07-23 17:31:01 -0400

    add fold expression example

---
+++ standard_library/fold_expression/fold.cpp
@@ -0,0 +1,36 @@
+// This program shows how to use fold expression in C++17
+// By: Nick from CoffeeBeforeArch
+
+#include <iostream>
+
+using namespace std;
+
+// We've seen variadic templates before, but now we don't
+// have to do the unpacking...
+template<typename ...Args>
+void prints(Args... args){
+    // Of the form: (init op ... op pack)
+    // Expands to: ((init op pack1) op pack2...)op packN
+    (cout << ... << args) << endl;
+}
+
+// We can do it to reduce something like a sum to a single line!
+template <typename ...Args>
+auto sum(Args ...args){
+    // Of the form: (... op pack)
+    // Expands to: ((pack1 op pack2) op ...) op packN
+    return (args + ...);
+    
+    // This is equivilant (but 0 can be any number)
+    // return (args + ... + 0);
+}
+
+int main(){
+    // Use our simple prints variadic template w/ folding
+    prints(1, " hello ", 3.14);
+
+    // Use our simple sum variadic template with folding
+    prints(sum(1, 2, 3, 4, 5));
+
+    return 0;
+}
