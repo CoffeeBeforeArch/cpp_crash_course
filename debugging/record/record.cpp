251db30c1adebeedb0d11d45c27d8103f4e39d1f
Nick <coffeebeforearch@gmail.com>
2019-07-26 16:15:16 -0400

    Add record example

---
+++ debugging/record/record.cpp
@@ -0,0 +1,27 @@
+// This program can be used to show off the record feature in GDB
+// By: Nick from CoffeeBeforeArch
+
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    // Number of numbers to add together
+    int N = 10;
+    int *sum = new int[1];
+    int temp;
+
+    // Add random numbers together in a loop
+    srand(time(NULL));
+    for(int i = 0; i < N; i++){
+        temp = rand() % 100;
+        sum[0] += temp;
+    }
+
+    // Print out the result
+    cout << "The sum is " << sum[0] << endl;
+
+    return 0;
+}
