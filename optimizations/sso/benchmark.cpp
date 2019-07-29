022886abe7d4fed9de28b952112356a4d3c555bf
Nick <CoffeeBeforeArch@gmail.com>
2019-07-28 21:52:09 -0400

    Add sso benchmarks

---
+++ optimizations/sso/benchmark.cpp
@@ -0,0 +1,30 @@
+// This program contains a simple benchmark for testing the how the SSO
+// helps performance on small strings
+
+#include <benchmark/benchmark.h>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+static void stringBench(benchmark::State &s){
+    // Get the number of characters for our string
+    int string_len = s.range(0);
+
+    // Vector for holding the strings
+    vector<string> v;
+    v.resize(10000);
+
+    // Now let's emplace back a ton of strings
+    while(s.KeepRunning()){
+        for(int i = 0; i < 10000; i++){
+            // Create the string of a specified size
+            string str(string_len, 'a');
+            v.push_back(str);
+        }
+    }
+}
+BENCHMARK(stringBench)->DenseRange(0,32);
+
+BENCHMARK_MAIN();
+
