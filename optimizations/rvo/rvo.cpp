ae2dbcc75b18d2403e712edf8c7f86332143c1c3
Nick <CoffeeBeforeArch@gmail.com>
2019-08-04 11:00:59 -0400

    Add rvo example

--- optimizations/rvo/rvo.cpp
+++ optimizations/rvo/rvo.cpp
@@ -9,23 +9,38 @@
 
 using namespace std;
 
+// Simple wrapper around a string object
 struct MyString{
     string s;
     MyString(string s1) : s(s1) {}
+    // If we copy, we want to know, so print something
     MyString(const MyString &s2){
         cout << "Copying the string!" << endl;
+        s = s2.s;
     }
 };
 
+// This function benefits from RVO
+// Functions with return values secretly have 1 argument
+// (where to store the return value).
+// If we know ahead of time what is going to be returned, don't
+// make a local version and copy it, make the string where it will
+// be copied in the first place.
 MyString rvo(){
     MyString ms("Hey there!");
     return ms;
 }
 
+// This function does not benefit from RVO
+// Which string we return depends on a condition, therefore can't be
+// known ahead of time.
+// Therefore we have to copy the object on a return
 MyString no_rvo(int condition){
-    MyString ms_1("Hey there!");
+    MyString ms_1("Hi, fella!");
     MyString ms_2("Ahoy!");
 
+    // Can't know which string will be returned ahead of time, so
+    // we can't elide the copy.
     if(condition){
         return ms_1;
     }else{
@@ -39,9 +54,11 @@ int main(){
     int condition = rand() % 2;
 
     // Get the string with RVO
+    cout << "Getting a string with RVO" << endl;
     MyString s1 = rvo();
 
     // Get the string without RVO
+    cout << "Getting a string without RVO" << endl;
     MyString s2 = no_rvo(condition);
 
     // Print the strings
