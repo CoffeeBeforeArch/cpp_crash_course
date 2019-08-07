15add004e9814ae084a1414de2cc00fc4d194d9c
Nick <CoffeeBeforeArch@gmail.com>
2019-08-07 18:57:38 -0400

    Minor changes to constexpr example

--- standard_library/constexpr/constexpr.cpp
+++ standard_library/constexpr/constexpr.cpp
@@ -17,22 +17,26 @@ struct my_struct{
 };
 
 // We can even have a recursive functions be calculated at compile time as well
-constexpr int fibonacci(int i){
+int fibonacci(int i){
     return (i <= 1) ? 1 : fibonacci(i - 1) + fibonacci(i - 2);
 }
 
 int main(){
     // Let's make some constexpr variables
     constexpr int i = 5;
-    constexpr int result = fibonacci(i);
     constexpr double d = 1.5;
+
+    // We can declare a struct with constexpr members
     my_struct example;
+
+    // We can even call a recursive function!
+    int result = fibonacci(i);
 
     // And lets print them out
     cout << "i = " << i << endl;
-    cout << "result = " << result << endl;
     cout << "d = " << d << endl;
-    cout << "my_sturct ID = " << example.ID << endl;
+    cout << "result = " << result << endl;
+    cout << "my_struct ID = " << example.ID << endl;
 
     return 0;
 }
