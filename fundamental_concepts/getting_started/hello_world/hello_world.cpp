23a6245f6451c609e8933569adeebe6d71ad72da
Nick <CoffeeBeforeArch@gmail.com>
2019-09-01 22:28:22 -0400

    re-format hellow_world.cpp

--- fundamental_concepts/getting_started/hello_world/hello_world.cpp
+++ fundamental_concepts/getting_started/hello_world/hello_world.cpp
@@ -1,11 +1,18 @@
-// Prints "Hello, world!"
+// Prints "Hello, World!" to the console using cout
 // By: Nick from CoffeeBeforeArch
 
+// Pastes the contents of file(s) where #include is located
 #include <iostream>
 
-using namespace std;
+// Use cout and endl from namespace std
+using std::cout;
+using std::endl;
 
-int main(){
-    cout << "Hello, World!\n";
+// All C++ program require a main functions (entry point to the program)
+int main() {
+    // Pass "Hello, World!" to the cout stream object
+    cout << "Hello, World!" << endl;
+
+    // Return 0 typically means we did not encounter an error
     return 0;
 }
