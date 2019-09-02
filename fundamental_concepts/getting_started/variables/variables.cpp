df0e3f7d758dac2519f88523141ad5e4c8c6157d
Nick <CoffeeBeforeArch@gmail.com>
2019-09-01 22:34:22 -0400

    Add string example

--- fundamental_concepts/getting_started/variables/variables.cpp
+++ fundamental_concepts/getting_started/variables/variables.cpp
@@ -1,28 +1,34 @@
-// This program initializes some variables and performs 
+// This program initializes some variables and performs
 // some arithmetic operations on them. It also prints a
 // string.
 // By: Nick from CoffeeBeforeArch
 
 #include <iostream>
+#include <string>
 
-using namespace std;
+using std::cout;
+using std::endl;
+using std::string;
 
-int main(){
+int main() {
     // Declare variables
     float a;
     float b;
     float sum;
-    const char* some_string = "Hey there, fella!\n";
 
     // Initialize variables
     a = 5.823;
     b = 10.123;
 
+    // Declare and initialize a string
+    string some_string = "Hey there, fella!";
+
     // Compute the sum
     sum = a + b;
 
     // Print string and sum
-    cout << some_string;
+    cout << some_string << endl;
     cout << "The sum of " << a << " and " << b << " is " << sum << endl;
+
     return 0;
 }
