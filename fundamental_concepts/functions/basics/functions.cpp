2ccd12c93f483812c95bee1d6283bc2a9b0bba82
Nick <CoffeeBeforeArch@gmail.com>
2019-09-12 12:19:54 -0400

    re-write function example

--- fundamental_concepts/functions/basics/functions.cpp
+++ fundamental_concepts/functions/basics/functions.cpp
@@ -4,53 +4,44 @@
 
 #include <iostream>
 
-using namespace std;
+using std::cout;
+using std::endl;
 
-// Prints the size of a character and updates value
-char print_char(char c) {
-  cout << "char value " << c << ", char size is " << sizeof(c) << endl;
-  return 'A';
+// Prints the value of an integer and it's size, and increments the value
+int print_and_inc_int(int i) {
+  cout << "Value: " << i << ", S: " << sizeof(i) << endl;
+  return i + 1;
 }
 
-// Prints the size of an integer and updated value
-int print_int(int i) {
-  cout << "int value " << i << ", int size is " << sizeof(i) << endl;
-  return 1234;
+// Prints the value of a float and it's size, and increments the value
+float print_and_inc_float(float sp) {
+  cout << "Value: " << sp << ", S: " << sizeof(sp) << endl;
+  return sp + 1;
 }
 
-// Prints the size of a single precision floating point number and updates value
-float print_float(float f) {
-  cout << "float value " << f << ", float size is " << sizeof(f) << endl;
-  return 12.832;
-}
-
-// Prints the size of a double precision floating point number and updates value
-double print_double(double d) {
-  cout << "double value " << d << ", double size is " << sizeof(d) << endl;
-  return 594.123;
+// Prints the value of a double and it's size, and increments the value
+double print_and_inc_double(double dp) {
+  cout << "Value: " << dp << ", S: " << sizeof(dp) << endl;
+  return dp + 1;
 }
 
 int main() {
   // Declare and initialize variables
-  char character = 'a';
-  int integer = 4362;
-  float single_precision = 20.123;
-  double double_precision = 194.421;
+  int i = 4362;
+  float sp = 20.123;
+  double dp = 194.421;
 
-  // Call functions
-  character = print_char(character);
-  integer = print_int(integer);
-  single_precision = print_float(single_precision);
-  double_precision = print_double(double_precision);
+  // Call our functions
+  // Return value overwrites our input argument (e.g. i becomes i + 1, returned
+  // from the function "print_and_inc_int(i)")
+  i = print_and_inc_int(i);
+  sp = print_and_inc_float(sp);
+  dp = print_and_inc_double(dp);
 
-  // Add space between old and new values
-  cout << endl;
-
-  // Print out new values
-  cout << "char value " << character << endl;
-  cout << "int value " << integer << endl;
-  cout << "float value " << single_precision << endl;
-  cout << "double value " << double_precision << endl;
+  // Print out the returned values
+  cout << "New Int value " << i << endl;
+  cout << "New Float value " << sp << endl;
+  cout << "New Double value " << dp << endl;
 
   return 0;
 }
