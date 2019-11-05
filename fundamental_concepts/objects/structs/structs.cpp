0e109b0670608e188d436ea7445aff7746893065
Nick <CoffeeBeforeArch@gmail.com>
2019-11-05 18:18:42 -0500

    Add structs dir

---
+++ fundamental_concepts/objects/structs/structs.cpp
@@ -0,0 +1,59 @@
+// This program shows off the basics of structs in C++
+// By: Nick from CoffeeBeforeArch
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+// Structs are user-defined data types!
+// They act as blueprints to create objects
+struct Wallet {
+  // Structs have access specifiers that say who is allowed to access which
+  // data/functions
+  // Private says this can only be accessed by members/friends of this struct
+  // (more on friends later)
+  // The default access specifier for structs is public
+ private:
+  // Structs can have fields (data members)
+  int dollars;
+  int cents;
+
+  // They can also have methods (functions associated with the struct)
+ public:
+  // We can have methods for printing
+  void print();
+
+  // 'setters' and 'getters' are common when we have private members we want to
+  // manipulate
+  void set_dollars(int d) { dollars = d; }
+  void set_cents(int c) { cents = c; }
+  int get_dollars() { return dollars; }
+  int get_cents() { return cents; }
+};
+
+// Methods are often implemented outside the struct (and in different files)
+// We do this to avoid redefinition errors when we are including files
+void Wallet::print() {
+  cout << "Value of wallet = $" << dollars << "." << cents << endl;
+}
+
+int main() {
+  // We can create a new instance of a struct (an object) like any other variable
+  Wallet nicks_wallet;
+
+  // We can't access the fields 'dollars' and 'cents' directly (they're
+  // private)! We can use 'setters' to set them and 'getters' to get them
+  nicks_wallet.set_dollars(10);
+  nicks_wallet.set_cents(37);
+
+  // We can not directly access the data fields because they are private
+  // int tmp_dollars = nicks_wallet.dollars; // FAILS!
+  // int tmp_cents = nicks_wallet.cents; //  FAILS!
+
+  // We can print out our custom string just by calling the method
+  // This will use the values in our object
+  nicks_wallet.print();
+
+  return 0;
+}
