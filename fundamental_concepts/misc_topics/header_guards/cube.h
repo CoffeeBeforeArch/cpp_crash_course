8262346ac9b7f26f4c296c30bd47237bdf2cd95f
Nick <CoffeeBeforeArch@gmail.com>
2019-12-24 21:57:26 -0500

    remove a bunch of examples that may not be features in version 2 of the series

--- fundamental_concepts/misc_topics/header_guards/cube.h
+++
@@ -1,5 +0,0 @@
-// This file contains a function for computing the square of a number
-// By: Nick from CoffeeBeforeArch
-#include "square.h"
-
-int cube(int a) { return square(a) * a; }
--- fundamental_concepts/misc_topics/header_guards/square.h
+++
@@ -1,6 +0,0 @@
-// This file contains a function for computing the square of a number
-// By: Nick from CoffeeBeforeArch
-#ifndef SQUARE_H
-#define SQUARE_H
-int square(int a) { return a * a; }
-#endif
