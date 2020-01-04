6e72c481b0baaa931c6cc6235b261301f5f8a4b0
Nick <CoffeeBeforeArch@gmail.com>
2020-01-03 19:00:24 -0500

    Fix destructors example

--- fundamental_concepts/objects/destructors/destructors.cpp
+++ fundamental_concepts/objects/destructors/destructors.cpp
@@ -9,38 +9,27 @@ using std::cout;
 // Our class from last example
 class IntArray {
  private:
-  int *data;
-  int size;
+  int *data = nullptr;
+  int size = 0;
 
  public:
   // Our constructor, default constructor, and and copy constructor
   IntArray(int N);
   IntArray() = delete;
-  IntArray(const IntArray &a) = default;
+  IntArray(const IntArray &a);
 
   // Our destructor!
   // Specifies what happens when we destroy an object
   ~IntArray();
 
   // Get the size of our array
-  int get_size() { return size; }
-  int *get_data() { return data; }
-  // Set all the elements in our array
-  void set(const int val) {
-    for (int i = 0; i < size; i++) {
-      data[i] = val;
-    }
-  }
+  int get_size() const { return size; }
 
-  // Get an element at a position
-  // Fails if the index is out of bounds
-  int at(int index) {
-    if (index < size) {
-      return data[index];
-    } else {
-      assert(0);
-    }
-  }
+  // Get a raw pointer to the data
+  int *get_data() const { return data; }
+
+  // Overloaded operator to index our data
+  int &operator[](unsigned id) { return data[id]; }
 };
 
 // Our constructor where we allocate memory
@@ -49,6 +38,22 @@ IntArray::IntArray(int N) {
   cout << "Allocating memory in constructor!\n";
   data = new int[N]();
   size = N;
+}
+
+// Our copy constructor that performs a deep copy!
+IntArray::IntArray(const IntArray &a) {
+  // Check if we need to re-allocate
+  int copy_n = a.get_size();
+  int *copy_ptr = a.get_data();
+  if (size != copy_n && data != nullptr) {
+    delete[] data;
+  }
+
+  // Copy the data
+  data = new int[copy_n];
+  for (int i = 0; i < copy_n; i++) {
+    data[i] = copy_ptr[i];
+  }
 }
 
 // Destructors have the name as the class they belong to, but use a tilde (~)
@@ -62,13 +67,15 @@ IntArray::~IntArray() {
 int main() {
   // Create an array with 10 elements
   IntArray a1(10);
-  a1.set(5);
+  for (int i = 0; i < a1.get_size(); i++) {
+    a1[i] = i;
+  }
 
   // Use the copy constructor
   IntArray a2 = a1;
 
-  // We just copied the pointer!
-  // This is known as a shallow copy, and can be dangerous if we de-allocate
+  // With our custom constructor, we can stop our pointers from aliasing each
+  // other!
   cout << "a1's pointer: " << a1.get_data()
        << " a2's pointer: " << a2.get_data() << '\n';
 
