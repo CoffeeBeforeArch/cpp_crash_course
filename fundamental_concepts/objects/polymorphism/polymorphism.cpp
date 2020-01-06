26b2a69770e41cc6b27d49cff33a11d7acd85ac7
Nick <CoffeeBeforeArch@gmail.com>
2020-01-06 14:21:39 -0500

    finish polymorphism example

--- fundamental_concepts/objects/polymorphism/polymorphism.cpp
+++ fundamental_concepts/objects/polymorphism/polymorphism.cpp
@@ -10,25 +10,45 @@ class NoInheritence {};
 // Our base class with a virtual function
 class Mammal {
  public:
-  virtual void speak() { std::cout << "I am a mammal!\n"; }
+  virtual void speak() const { std::cout << "I am a mammal!\n"; }
 };
 
 // Our first inherited class
-class Dog : Mammal {
+class Dog : public Mammal {
  public:
-  void speak() { std::cout << "I am a mammal, but also a dog!\n"; }
+  void speak() const override {
+    std::cout << "I am a mammal, but also a dog!\n";
+  }
 };
 
 // Our second inherited class
-class Cat : Mammal {
+class Cat : public Mammal {
  public:
-  void speak() { std::cout << "I am a mammal, but also a cat!\n"; }
+  void speak() const override {
+    std::cout << "I am a mammal, but also a cat!\n";
+  }
 };
+
+// We can treat objects of different types in a uniform way!
+void callFunc(Mammal &m) {
+  m.speak();
+}
 
 int main() {
   // Print the size of classes w/ and w/o polymorphism
   std::cout << "sizeof(NoInheritence) = " << sizeof(NoInheritence) << '\n';
   std::cout << "sizeof(Mammal) = " << sizeof(Mammal) << '\n';
+
+  // Let's create three objects
+  Mammal m;
+  Dog d;
+  Cat c;
+
+  // We Pass all three to a function which implicitly upcasts the objects
+  // Each will still call the correct method (dynamic dispatch)
+  callFunc(m);
+  callFunc(d);
+  callFunc(c);
 
   return 0;
 }
