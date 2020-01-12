1b14086968442d0458b7d88289b62b8771de5f23
Nick <CoffeeBeforeArch@gmail.com>
2020-01-12 13:16:16 -0500

    remove bad namespace std

--- optimizations/sso/benchmark.cpp
+++ optimizations/sso/benchmark.cpp
@@ -5,21 +5,19 @@
 #include <string>
 #include <vector>
 
-using namespace std;
-
 static void stringBench(benchmark::State &s) {
   // Get the number of characters for our string
   int string_len = s.range(0);
 
   // Vector for holding the strings
-  vector<string> v;
+  std::vector<std::string> v;
   v.reserve(10000);
 
   // Now let's push back a ton of strings
   while (s.KeepRunning()) {
     for (int i = 0; i < 10000; i++) {
       // Create the string of a specified size
-      string str(string_len, 'a');
+      std::string str(string_len, 'a');
       v.push_back(str);
     }
   }
--- optimizations/sso/sso.cpp
+++ optimizations/sso/sso.cpp
@@ -6,23 +6,21 @@
 #include <iostream>
 #include <string>
 
-using namespace std;
-
 // Overload new operator to track heap allocations
 void* operator new(size_t n) {
-  cout << "Allocating " << n << " bytes" << endl;
+  std::cout << "Allocating " << n << " bytes\n";
   return malloc(n);
 }
 
 int main() {
   // First, let's see how big a string is
-  size_t string_size = sizeof(string);
-  cout << "Size of string = " << string_size << endl;
+  size_t string_size = sizeof(std::string);
+  std::cout << "Size of string = " << string_size << '\n';
 
   // Gradually increase the size of the string in the loop
   for (size_t i = 0; i < 32; i++) {
-    string s(i, 'X');
-    std::cout << i << ": " << s << std::endl;
+    std::string s(i, 'X');
+    std::cout << i << ": " << s << '\n';
   }
 
   return 0;
