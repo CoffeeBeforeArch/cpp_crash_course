2323db4112e84240585ff6993c5dd840d1d7f28f
Nick <CoffeeBeforeArch@gmail.com>
2020-01-15 19:50:23 -0500

    couple extra comment fixes

--- fundamental_concepts/objects/inheritance/inheritance.cpp
+++ fundamental_concepts/objects/inheritance/inheritance.cpp
@@ -5,9 +5,9 @@
 #include <string>
 
 // Inheritance allows us to re-use code for classes and structs!
-// If we have multiple classes that share the same data members and methods, we
-// can implement those a single time as a parent class, and inherit from that
-// class!
+// If we have multiple classes/structs that share the same data members and
+// methods, we can implement those a single time as a parent class/struct, and
+// inherit from that class/struct!
 
 // Here, we create a simple struct with a single std::string
 struct Parent {
@@ -19,8 +19,8 @@ struct Parent {
 };
 
 // Our Child struct inherits from the Parent struct
-// We can expand on the parent struct, and implement things specific to the child
-// class
+// We can expand on the parent struct, and implement things specific to the
+// child struct
 struct Child : Parent {
   // We can use an initialization list to call our parents constructor
   // These can automatically called before we construct a Child object
@@ -34,8 +34,8 @@ struct Child : Parent {
 int main() {
   // Let's look at the sizes of different object
   // The string object in modern versions of GCC is 32 Bytes
-  // Our Parent class with one string ends up being the size of the string
-  // Our Child class ends up having the size of two strings!
+  // Our Parent struct with one string ends up being the size of the string
+  // Our Child struct ends up having the size of two strings!
   std::cout << "sizeof(std::string) = " << sizeof(std::string) << '\n';
   std::cout << "sizeof(Parent) = " << sizeof(Parent) << '\n';
   std::cout << "sizeof(Child) = " << sizeof(Child) << '\n';
