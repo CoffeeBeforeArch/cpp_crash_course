8a31bec2af334e28af0ca28042adc712092a2a45
Nick <CoffeeBeforeArch@gmail.com>
2020-01-26 11:27:12 -0500

    Add re-write example

--- fundamental_concepts/objects/templates/class_templates.cpp
+++ fundamental_concepts/objects/templates/class_templates.cpp
@@ -1,87 +1,63 @@
-// This program shows off the basics of constructors in C++
+// This program shows off the basics of templated classes in C++
 // By: Nick from CoffeeBeforeArch
 
-#include <cassert>
 #include <iostream>
 
 using std::cout;
 
-// Our class from last example
+// A simple templated class that holds a dynamically allocated array
 template <class T>
-class Array {
- private:
-  T *data = nullptr;
-  int size = 0;
-
+class Data {
  public:
-  // Our constructor, default constructor, and and copy constructor
-  Array(int N);
-  Array() = delete;
-  Array(const Array &a);
-  ~Array();
+  // Disable default constructor and copying
+  Data() = delete;
+  Data(const Data &d) = delete;
+  Data &operator=(const Data &rhs) = delete;
 
-  // Get the size of our array
-  int get_size() const { return size; }
+  // We'll implement our own custom constructor destructor, and indexing
+  Data(int N);
+  ~Data();
+  T &operator[](const unsigned i);
 
-  // Our templated methods
-  T *get_data() const { return data; }
-  T &operator[](unsigned id) { return data[id]; }
+ private:
+  // A pointer to our data
+  T *d;
 };
 
-// Our constructor where we allocate memory
+// Our templated constructor
 template <class T>
-Array<T>::Array(int N) {
-  // Memory is also value initialized
-  cout << "Allocating memory in constructor!\n";
-  data = new T[N]();
-  size = N;
+Data<T>::Data(int N) {
+  d = new T[N]();
 }
 
-// Our copy constructor that performs a deep copy!
+// Our templated destructor
 template <class T>
-Array<T>::Array(const Array &a) {
-  // Check if we need to re-allocate
-  int copy_n = a.get_size();
-  T *copy_ptr = a.get_data();
-  if (size != copy_n && data != nullptr) {
-    delete[] data;
-  }
-
-  // Copy the data
-  data = new T[copy_n];
-  for (int i = 0; i < copy_n; i++) {
-    data[i] = copy_ptr[i];
-  }
+Data<T>::~Data() {
+  cout << "Printing from the basic destructor!\n";
+  delete[] d;
 }
 
-// Our general destructor for arbitrary types
+// Our templated indexing operator
 template <class T>
-Array<T>::~Array() {
-  cout << "Freeing our allocated memory in destructor!\n";
-  delete[] data;
+T &Data<T>::operator[](const unsigned i) {
+  return d[i];
 }
 
-// Our specialized destructor for floating point numbers
+// Our specialized destructor!
+// Allows us to treat some types differently than the general case
 template <>
-Array<float>::~Array() {
-  cout << "Hello from the float destructor!\n";
-  delete[] data;
+Data<float>::~Data() {
+  cout << "Printing from the float destructor!\n";
+  delete[] d;
 }
 
 int main() {
-  // Create an array with 10 elements
-  Array<float> a1(10);
-  for (int i = 0; i < a1.get_size(); i++) {
-    a1[i] = i;
-  }
+  // Create some Data objects of with different types
+  Data<int> d1(10);
+  Data<char> d3(10);
+  Data<bool> d4(10);
+  Data<float> d2(10);
 
-  // Use the copy constructor
-  Array<float> a2 = a1;
-
-  // With our custom constructor, we can stop our pointers from aliasing each
-  // other!
-  cout << "a1's pointer: " << a1.get_data()
-       << " a2's pointer: " << a2.get_data() << '\n';
-
+  // Destructors fire when we return!
   return 0;
 }
