5d43426ee89d8692d56da540014d2896c5f3a94b
Nick <CoffeeBeforeArch@gmail.com>
2020-01-31 22:40:44 -0800

    Add stl sorting example

---
+++ algorithms/sorting/stl/stl_sort.cpp
@@ -0,0 +1,42 @@
+// This program shows off STL sort in C++
+// By: Nick from CoffeeBeforeArch
+
+#include <algorithm>
+#include <iostream>
+#include <random>
+#include <vector>
+
+int main() {
+  // Number of elements to sort
+  const unsigned N = 20;
+
+  // Our vector to sort
+  std::vector<int> v(N);
+
+  // Set up our random number generator
+  std::mt19937 rng;
+  rng.seed(std::random_device()());
+  std::uniform_int_distribution<int> dist(0, 255);
+
+  // Generate our random inputs
+  std::generate(begin(v), end(v), [&]() { return dist(rng); });
+
+  // Dump the potentially unsorted contents
+  std::cout << "Before: ";
+  for (auto i : v) {
+    std::cout << i << " ";
+  }
+  std::cout << '\n';
+
+  // Sort the vector
+  std::sort(begin(v), end(v));
+
+  // Dump the sorted contents
+  std::cout << "After: ";
+  for (auto i : v) {
+    std::cout << i << " ";
+  }
+  std::cout << '\n';
+
+  return 0;
+}
