fb6b82e9076d05987c9540ef298b8baeb1a4616d
Nick <coffeebeforearch@gmail.com>
2022-03-20 23:56:34 -0500

    Add multiple print examples

--- src/01_printing/iostream.cpp
+++ src/01_printing/iostream.cpp
@@ -1,14 +1,10 @@
 // Prints "Hello, World!" to the console using cout
 // By: Nick from CoffeeBeforeArch
 
-// Pastes the contents of file(s) where #include is located
 #include <iostream>
 
-// All C++ program require a main functions (entry point to the program)
 int main() {
-  // Pass "Hello, World!" to the cout stream object
   std::cout << "Hello, World!\n";
 
-  // Returning 0 means we did not encounter an error
   return 0;
 }
---
+++ src/01_printing/printf.cpp
@@ -0,0 +1,10 @@
+// Prints "Hello, World!" to the console using printf
+// By: Nick from CoffeeBeforeArch
+
+#include <cstdio>
+
+int main() {
+  printf("Hello, World!\n");
+
+  return 0;
+}
